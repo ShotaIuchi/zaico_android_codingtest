9c1423bc54d97c77cfac4c48f52eb33c759aa8d1
Shota Iuchi <shotaiuchi.develop@gmail.com>
2025-04-18 22:45:59 +0900

    プロジェクト作成＋ignore設定改造

---
+++ app/src/main/java/jp/co/zaico/codingtest/MainActivity.kt
@@ -0,0 +1,47 @@
+package jp.co.zaico.codingtest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import jp.co.zaico.codingtest.ui.theme.Zaico_android_codingtestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Zaico_android_codingtestTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Zaico_android_codingtestTheme {
+        Greeting("Android")
+    }
+}
